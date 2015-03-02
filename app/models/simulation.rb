5bfcd32c91ff514c0b0673a30ba2baafc60c79d3
vandanaptotem <vandana.ptotem@gmail.com>
2015-03-02 11:15:40 +0530

    application added

--- app/models/simulation.rb
+++ app/models/simulation.rb
@@ -2,10 +2,9 @@ class Simulation < ActiveRecord::Base
 
   has_attached_file :excel_file#, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 
-  # validates_attachment_content_type :excel_file, :content_type => ["application/xls","application/xlsx"]
+  #validates_attachment_content_type :excel_file, :content_type => ["application/xls","application/xlsx"]
   validates_attachment_content_type :excel_file,
   :content_type => ["application/pdf","application/vnd.ms-excel",
-                    "application/xls","application/xlsx",
                     "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
                     "application/msword",
                     "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
